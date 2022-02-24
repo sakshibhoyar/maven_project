f76cdb5487f54b488b474302ca0cdbf9585ecd26
sakshibhoyar <100134989+sakshibhoyar@users.noreply.github.com>
2022-02-24 12:15:22 +0000

    practicing on maven projects

---
+++ profilesmaven/src/test/java/com/bharat/maven/profilesmaven/AppTest.java
@@ -0,0 +1,20 @@
+package com.bharat.maven.profilesmaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
